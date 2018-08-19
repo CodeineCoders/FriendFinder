f9be1f407a395f56b57a61efdfdb90c36fc7d039
Brian Racek <rac3k@aol.com>
2018-08-19 11:23:31 -0400

    added questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,68 +1,51 @@
 var friends = [
     {
-      name: "Ahmed",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      scores: [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-
-    {
-        name: "Jane",
+        name: "Bugs Bunny",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: [
           "5",
           "1",
+          "5",
+          "5",
           "4",
-          "4",
+          "1",
           "5",
           "1",
-          "2",
-          "5",
-          "4",
+          "1",
           "1"
         ]
       },
 
       {
-        name: "John",
+        name: "Elmer Fudd",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: [
+          "1",
+          "3",
+          "1",
+          "3",
+          "1",
+          "3",
+          "3",
           "5",
           "1",
-          "4",
-          "4",
-          "5",
-          "1",
-          "2",
-          "5",
-          "4",
-          "1"
+          "5"
         ]
       },
 
       {
-        name: "Bob",
+        name: "Daffy Duck",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: [
+          "1",
           "5",
           "1",
-          "4",
-          "4",
+          "1",
+          "5",
           "5",
           "1",
-          "2",
+          "1",
           "5",
-          "4",
           "1"
         ]
       },
